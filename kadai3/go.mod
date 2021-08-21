13f3f3cda199c1b2596e5dd9b2464474d727ffe8
Junichi Nishi <github@purewest.dip.jp>
2021-08-21 18:03:39 +0900

    課題3未完了 3-3 難しい！！！ 最後まで考えます！

---
+++ kadai3/go.mod
@@ -0,0 +1,3 @@
+module kadai3
+
+go 1.16
---
+++ kadai3/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type Turtle struct {
+	name string
+	x    float64
+	y    float64
+	a    float64
+}
+
+// 課題3-1
+func (t *Turtle) move(a float64) {
+	t.a -= a
+}
+
+// 課題3-2
+func (n *Turtle) moven(name string) {
+	n.name = name
+}
+func (k *Turtle) movek(name string) {
+	k.name += ":こんにちは！"
+}
+func main() {
+	// structの宣言の仕方
+	var t1 Turtle = Turtle{"師匠", 1000, 5, 180.5}
+	var t2 Turtle = Turtle{"弟子", 10, 250, 270.3}
+	var k1 Turtle = Turtle{}
+	fmt.Println(t1)
+	fmt.Println(t2)
+	// 課題3-1
+	t1.move(35)
+	fmt.Println(t1)
+	// 課題3-2
+	t2.moven("西")
+	fmt.Println(t2)
+	k1.movek("ふうた先生")
+	fmt.Println(k1)
+
+}
